71bbd9d36903703005f709fd1a8506c89edb053f
n3wjack <n3wjack@n3wjack.net>
2022-07-05 20:55:58 +0200

    Switch build script to Powershell.

--- build.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-echo.
-echo **** Building self-contained winx64 exectable. ****
-echo.
-
-dotnet publish -p:PublishSingleFile=true -r win-x64 -c Release --self-contained true -p:PublishTrimmed=true
-
-echo **** Executable can be found in \bin\Release\net6.0\win-x64\publish\ ****
